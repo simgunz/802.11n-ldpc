a76841479ec2bb950119565014d28d9c5c9eef0b
Simone Gaiarin <simgunz@gmail.com>
2012-08-06 13:39:25 +0200

    v1.0.1

---
+++ plotLDPC.m
@@ -0,0 +1,34 @@
+function [ ] = plotLDPC( x, ber_ldpc, fer_ldpc, varargin)
+%PLOTLDPC Summary of this function goes here
+%   Detailed explanation goes here
+
+berColor = {'--sb','--+r','--dg','--om'};
+ferColor = {'-sb','-+r','-dg','-om'};
+
+f = figure;
+
+if length(varargin)
+    iteration = x;    
+    semilogy(iteration,fer_ldpc);        
+    xlabel('Number of iteration');
+    ylabel('FER');
+    saveas(f,'output/FERvsITER');
+    saveas(f,'output/FERvsITER','pdf');
+else
+    EbN0dB = x;        
+    semilogy(EbN0dB,ones(1,length(ber_ldpc)));
+    hold on;
+    for i=1:length(ber_ldpc(1,1,:))
+        plot(EbN0dB,ber_ldpc(:,:,i),berColor{i}); 
+        plot(EbN0dB,fer_ldpc(:,:,i),ferColor{i}); 
+    end
+    hold off;    
+    legend('LDPC Simulated BER','Bad LDPC Simulated BER','Uncoded BER');
+    xlabel('Eb/N0 [dB]');
+    ylabel('BER/FER');
+    saveas(f,'output/BEReFERvsEbN0');
+    saveas(f,'output/BEReFERvsEbN0','pdf');
+end
+
+end
+
