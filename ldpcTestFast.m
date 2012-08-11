ed61d12fb9761499709cc299be7b257a22eedfcb
Simone Gaiarin <simgunz@gmail.com>
2012-08-11 15:04:22 +0200

    Add fast decoder

---
+++ ldpcTestFast.m
@@ -0,0 +1,118 @@
+clc;
+clear all;
+close all;
+
+%% TUNABLE PARAMETERS %%
+
+backSubstitution = 1;       % Enable encoding by back substitution
+
+% Parameters presets:
+% 0: BER vs iteration test
+% 1: Rate 1/2 accurate BER and FER test
+% 2: Rate comparison, less accurate BER and FER test
+% 3: Manual
+preset = 0;
+
+% Manual mode parameters
+mu = 10^6;               % Input length
+R = 1/2;                 % Code rate. Available rates 1/2, 2/3, 3/4, 5/6
+iter = 1 ;               % Number of simulations (100 for good results)
+EbN0step = 0.25;         % Set to 0.5 to speed things up
+EbN0dB = 1:EbN0step:2.5; %Eb/N0 values
+
+
+%% PRESET %%
+
+% switch preset
+%     case 0,
+%         mu = 10^6;
+%         R = 1/2;
+%         iter = 100;
+%         ldpcIter = 1:50;
+%         EbN0dB = 1.5;
+%     case 1,
+%         mu = 10^7;
+%         R = 1/2;
+%         iter = 50;
+%         EbN0dB = 1:EbN0step:3;                
+%     case 2,
+%         mu = 10^7;
+%         R = 1/2;
+%         iter = 2;
+%         EbN0dB = 1:EbN0step:2.5;        
+%     case 3,
+%         mu = 10^6;
+%         R = [1/2, 2/3, 3/4, 5/6];
+%         iter = 1;
+%         EbN0dB = 1:EbN0step:2.25;        
+% end
+
+switch preset
+    case 0,                 % 2 giorni
+        mu = 10^6;
+        R = 1/2;
+        iter = 100;
+        ldpcIter = 1:50;
+        EbN0dB = 1.5;
+    case 1,                 % 34 ore
+        mu = 10000;
+        R = 1/2;
+        iter = 100;
+        EbN0dB = 1.5;                       
+    case 2,
+        mu = 10^7;
+        R = [1/2, 2/3, 3/4, 5/6];
+        iter = 1;
+        EbN0dB = 1:EbN0step:2.25;        
+end
+
+
+%% SIMULATION %%
+
+tic
+if ~preset
+    ber_ldpc = zeros(length(ldpcIter),iter);
+    fer_ldpc = zeros(length(ldpcIter),iter);
+    gammaDB = EbN0dB + 10*log(2*R);
+    for i=1:length(ldpcIter)
+        for j=1:iter
+            u_input = round(rand(1,mu));       % Random input sequence
+            [u_output, ber_ldpc(i,j), fer_ldpc(i,j)] = ldpcTxSystemFast( u_input, R, gammaDB, backSubstitution, ldpcIter(i));
+        end    
+        disp(ldpcIter(i));
+    end    
+    ber_ldpc = sum(ber_ldpc,2)/iter;
+    fer_ldpc = sum(fer_ldpc,2)/iter;            
+else
+    ber_ldpc = zeros(length(R),length(EbN0dB),iter);
+    fer_ldpc = zeros(length(R),length(EbN0dB),iter);
+    for i=1:length(R)
+        gammaDB = EbN0dB + 10*log(2*R(i));
+        for j=1:length(gammaDB)
+            for k=1:iter                
+                u_input = round(rand(1,mu));       % Random input sequence
+                [u_output, ber_ldpc(i,j,k), fer_ldpc(i,j,k)] = ldpcTxSystemFast( u_input, R(i), gammaDB(j), backSubstitution);                
+            end    
+            disp(j);
+        end
+    end    
+    ber_ldpc = sum(ber_ldpc,3)/iter;
+    fer_ldpc = sum(fer_ldpc,3)/iter;
+end
+time = toc        
+
+
+%% SAVE DATA %%
+if ~exist('output','dir')
+    mkdir('output');
+end
+save('output/workspace');
+
+
+%% PLOT %%
+
+if ~preset
+    plotLDPC(ldpcIter, ber_ldpc, fer_ldpc, 1);
+else
+    plotLDPC(EbN0dB, ber_ldpc, fer_ldpc);
+end
---
+++ ldpcTxSystemFast.m
@@ -0,0 +1,116 @@
+function [ u_output, ber, fer ] = ldpcTxSystemFast( u_input, R, gammaDB, backSubstitution, varargin)
+%LDPCTXSYSTEMFAST Simulates a 802.11 transmission system with ldpc encoding
+
+if length(varargin)
+    iterations = varargin{1};
+else
+    iterations = 25;
+end
+
+mu = length(u_input);            % Input length
+
+% Codeword length available in the standard: 648, 1296, 1944
+% 648, 1296 are used only when there are few input bits
+% 1944 used in all other cases (will be used here)
+
+n = 1944;   % Codeword length
+k = n*R;    % Payload length
+
+%%% PARITY CHECK MATRIX %%%%
+
+if backSubstitution
+    [H,Z] = buildH(n,R);
+else
+    [H,G,Z] = buildHG(n,R);
+end
+
+% To improve performance instead of using cell, a special matrix is
+% used. The first element of each row contains the number of useful element
+% in each row starting from index 2
+A = zeros(n,23);          % Row i contains the indexes of the check nodes in which the variable i is involved
+B = zeros(n-k,13);        % Row i contains the indexes of the variable nodes involved in the check i
+
+for vn=1:n
+    row = find(H(:,vn));
+    len = length(row);
+    A(vn,1) = len;
+    A(vn,2:len+1) = row;
+end
+for cn=1:(n-k)
+    row = find(H(cn,:));
+    len = length(row);
+    B(cn,1) = len;
+    B(cn,2:len+1) = row;
+end
+
+
+%%%%%%% ENCODER %%%%%%%
+
+nCW = ceil(mu/k);                   % Number of codewords
+Npad = nCW*k - mu;                  % Number of padding bits
+u_in = [ u_input zeros(1,Npad) ];
+
+if backSubstitution
+    c = zeros(n,nCW);
+    H1 = H(:,1:k);
+    H2 = H(:,(k+1):(k+Z));    
+    
+    for i=1:nCW
+        payload = u_in((i-1)*k+1:i*k)';        
+        s = H1 * payload;
+        s = reshape(s,Z,(n-k)/Z);
+        p1 = mod(sum(s,2),2);
+        temp = H2 * p1;
+        temp = reshape(temp,Z,(n-k)/Z);
+        stilda = mod(s + temp,2);
+        prevp2 = 0;
+        ll = length(stilda(1,:)) - 1;
+        p2 = zeros(ll*Z,1);
+        for j=1:ll
+            prevp2 = mod(stilda(:,j) + prevp2,2);
+            p2((j-1)*Z+1:j*Z) = prevp2;            
+        end
+        c(:,i) = [ payload; p1; p2];
+    end
+    %cc = toc;
+else    
+    c = mod(G*reshape(u_in,k,length(u_in)/k),2);    
+end
+
+
+%%%%%%%%% P/S %%%%%%%%%
+
+d = c(:)';
+
+
+%%% BPAM MODULATOR %%%%
+
+sTx = d;
+sTx(sTx==0) = -1;    % Map 0 to -1 and produce the transmitted signal
+
+
+%%%%%%% CHANNEL %%%%%%%
+
+r = awgn(sTx,gammaDB);          % Received signal
+
+
+%%%%%%%%% S/P %%%%%%%%%
+
+r = reshape(r,n,length(r)/n);
+
+
+%%% MESSAGE PASSING DECODER %%%
+
+sigmaw2 = 1/(10^(gammaDB/10));
+        
+[u_out, checkOK] = mexfastdecoder(k,n,nCW,sigmaw2,A,B,H,2*r/sigmaw2,iterations);
+
+% Remove the padding bits
+u_output = u_out(1:mu);
+
+% Compute bit error rate and frame error rate
+ber = sum(u_output~=u_input)/mu;
+fer = 1 - checkOK/nCW;
+end
+
+
